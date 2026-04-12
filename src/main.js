3a57226c2eda764298d2aec4338e85dfed205c9a
juaninci <mamanijuan.arq@gmail.com>
2026-04-12 02:56:22 -0400

    !!!

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import App from './components/libros.vue'
+import App from './components/AddBookModal.vue'
+import App from './components/Busqueda.vue'
+
+
+
+
 
 createApp(App).mount('#app')
